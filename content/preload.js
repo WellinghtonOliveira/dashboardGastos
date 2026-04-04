6c0e6fcb0a9702262146551bc6ca18f60b4dc2c6
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-04-04 18:23:27 -0300

    Update preload.js

--- content/preload.js
+++ content/preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  enableClick: () => ipcRenderer.send('enable-click'),
-  disableClick: () => ipcRenderer.send('disable-click')
+  // enableClick: () => ipcRenderer.send('enable-click'),
+  // disableClick: () => ipcRenderer.send('disable-click')
 });
