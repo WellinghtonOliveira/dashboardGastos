d81c571ac9c83493f709c4b3b14c7d9518ce1fc1
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-04-30 22:17:52 -0300

    Delete preload.js

--- dashboard/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Carregar todas as transações
-  loadTransactions: () => ipcRenderer.invoke('load-transactions'),
-  
-  // Listener para quando dados são atualizados
-  onTransactionsUpdated: (callback) => ipcRenderer.on('transactions-updated', callback)
-});
